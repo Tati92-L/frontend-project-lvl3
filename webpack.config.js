bbc4f8091e4c9237ee17dc283c9e95a85c79be85
Tati92-L <77683219+Tati92-L@users.noreply.github.com>
2022-02-08 21:35:39 +0300

    try 1

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
       template: 'index.html',
